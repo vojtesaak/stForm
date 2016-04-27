ca1ba3dbe6c4f866df346e131f7c7125750d8d78
vojtechMalek <vojtech.malek@gmail.com>
2016-04-27 14:50:09 +0200

    update

--- src/js/fields/fieldSelect.js
+++ src/js/fields/fieldSelect.js
@@ -9,7 +9,8 @@ var Field = require('./field');
 var Select = require('../utils/select');
 
 require('bootstrap-select-js');
-require('bootstrap-select-less/bootstrap-select.less!');
+//require('bootstrap-select-less/bootstrap-select.less!');
+require('bootstrap-select-css/bootstrap-select.css!');
 require('jquery.scrollto');
 
 var visible = require('visible-element')($);
